f4bfb6a1e06712292da42011fa42e4b84046f053
allan Kiche <allankiche18@gmail.com>
2022-09-24 14:45:26 +0300

    dynamic routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router-dom'
 import About from './components/About';
+import Admin from './components/Admin';
 import FeaturedProducts from './components/FeaturedProducts';
 import Home from './components/Home'
 import Navbar from './components/Navbar';
@@ -7,13 +8,15 @@ import NewProducts from './components/NewProducts';
 import NoMatch from './components/NoMatch';
 import OrderSummary from './components/OrderSummary';
 import Products from './components/Products';
+import UserDetails from './components/UserDetails';
+import Users from './components/Users';
 
 function App() {
   return (
     <>
     <Navbar />
     <Routes>
-      < Route path ='/' element ={<Home/>}/>
+      <Route path ='/' element ={<Home/>}/>
       <Route path ='about' element = {<About/>}/>
       <Route path='order-summary' element ={<OrderSummary/>}/>
       <Route path='products' element = {<Products />}>
@@ -21,9 +24,13 @@ function App() {
         <Route path ='featured' element ={<FeaturedProducts/>}/>
         <Route path='new' element ={<NewProducts/>}/>
       </Route>
+      <Route path='users' element = {<Users/>}>
+        <Route path=':userId' element ={<UserDetails/>}/>
+        <Route path='admin' element ={<Admin/>}/>
+      </Route>
       <Route path='*' element ={<NoMatch />}/>
-
     </Routes>
+    
     </>
   );
 }
---
+++ src/components/UserDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function UserDetails() {
+  return (
+    <div>
+        <h1>User Details</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, obcaecati! Recusandae, aliquam nemo doloremque libero nam quis dicta magnam fugit sint natus cupiditate! Dolores maiores perspiciatis quis nisi! Quisquam, ipsa.</p>
+        </div>
+  )
+}
