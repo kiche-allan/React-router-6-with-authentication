ac67221b542eb0f0373262b22e08364f381a41a7
allan Kiche <allankiche18@gmail.com>
2022-09-24 15:06:59 +0300

    search params

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function UserDetails() {
+    const {userId} = useParams()
   return (
     <div>
-        <h1>User Details</h1>
+        <h1>Details about user {userId}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, obcaecati! Recusandae, aliquam nemo doloremque libero nam quis dicta magnam fugit sint natus cupiditate! Dolores maiores perspiciatis quis nisi! Quisquam, ipsa.</p>
         </div>
   )
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useSearchParams } from 'react-router-dom'
 
 export default function Users() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const showActiveUsers = searchParams.get('filter') === 'active'
   return (
     <div>
         <h2>User 1</h2>
         <h2>User 2</h2>
         <h2>User 3</h2>
         <Outlet/>
+        <div>
+            <button onClick={() => setSearchParams({ filter: 'active'})}>Active Users</button>
+            <button onClick={() => setSearchParams({})}>Reset Filters</button>
+        </div>
+        {
+            showActiveUsers ? ( <h2>Showing Active Users</h2> ) :(<h2>Showing all users</h2>)
+        }
     </div>
   )
 }
