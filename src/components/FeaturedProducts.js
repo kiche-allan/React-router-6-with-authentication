af3e438c05da1cbb5e273c0769c85db3721d4f8e
allan Kiche <allankiche18@gmail.com>
2022-09-24 13:27:42 +0300

    added an index route

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -5,7 +5,7 @@ export default function FeaturedProducts() {
     const navigate = useNavigate()
   return (
     <>
-    <div>List of New Products</div>
+    <div>List of Featured Products</div>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, accusamus ducimus aspernatur hic quibusdam molestiae animi dignissimos quam ad fugit et in officia nisi sunt maiores facilis sed cum numquam.</p>
     <button onClick={() => navigate(-1)}>Go Back</button>
     </>
